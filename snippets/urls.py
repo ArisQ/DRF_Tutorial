0333af4e5a58ce4996e92e01656b53a400b46e6f
ArisQ <qiaoli.pg@gmail.com>
2018-08-16 11:46:04 +0800

    add pastebin api

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from snippets import views
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
 ]
